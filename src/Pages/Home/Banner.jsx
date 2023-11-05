372f918e933789de8a7bab9e12b2c9f59984d48e
tafiya <tafiapinkey@gmail.com>
2023-11-05 14:16:42 +0600

    home page layout build

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,74 @@
+import img1 from '../../assets/image/pexels-andrea-piacquadio-3769021.jpg';
+import img2 from '../../assets/image/pexels-ivan-samkov-4458554.jpg';
+import img3 from '../../assets/image/pexels-julia-m-cameron-4144294.jpg';
+import img4 from '../../assets/image/slide4.jpg';
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[600px]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={img1} className="w-full rounded-xl" />
+          <div className="absolute flex rounded-xl items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] h-full left-0  top-0">
+            <div className='text-white w-2/3 pl-12 md:ml-24'>
+              <h1 className=' text-6xl font-bold mb-6'>Lets Grow <br></br> <span className=' text-yellow-500'>your Education</span>  <br /> Level Up</h1>
+              <p className='mb-6'>Hey! would you link to grow up your skill and if you are interested to do just start here</p>
+              <button className="btn   btn-secondary mr-5">Discover More</button>
+              <button className="btn btn-outline  btn-secondary ">Latest Project</button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={img2} className="w-full rounded-xl" />
+          <div className="absolute flex rounded-xl items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] h-full left-0  top-0">
+          <div className='text-white w-2/3 pl-12 md:ml-24'>
+              <h1 className=' text-6xl font-bold mb-6'>Lets Grow <br></br> <span className=' text-yellow-500'>your Education</span>  <br /> Level Up</h1>
+              <p className='mb-6'>Hey! would you link to grow up your skill and if you are interested to do just start here</p>
+              <button className="btn   btn-secondary mr-5">Discover More</button>
+              <button className="btn btn-outline  btn-secondary ">Latest Project</button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide1" className="btn btn-circle mr-5">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={img3} className="w-full rounded-xl" />
+          <div className="absolute flex rounded-xl items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] h-full left-0  top-0">
+          <div className='text-white w-2/3 pl-12 md:ml-24'>
+              <h1 className=' text-6xl font-bold mb-6'>Lets Grow <br></br> <span className=' text-yellow-500'>your Education</span>  <br /> Level Up</h1>
+              <p className='mb-6'>Hey! would you link to grow up your skill and if you are interested to do just start here</p>
+              <button className="btn   btn-secondary mr-5">Discover More</button>
+              <button className="btn btn-outline  btn-secondary ">Latest Project</button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide2" className="btn btn-circle mr-5">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src={img4} className="w-full rounded-lg" />
+          <div className="absolute flex  rounded-xl items-center bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] h-full left-0  top-0">
+          <div className='text-white w-2/3 pl-12 md:ml-24'>
+              <h1 className=' text-6xl font-bold mb-6'>Lets Grow <br></br> <span className=' text-yellow-500'>your Education</span>  <br /> Level Up</h1>
+              <p className='mb-6'>Hey! would you link to grow up your skill and if you are interested to do just start here</p>
+              <button className="btn   btn-secondary mr-5">Discover More</button>
+              <button className="btn btn-outline  btn-secondary ">Latest Project</button>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a href="#slide3" className="btn btn-circle mr-5">❮</a> 
+            <a href="#slide1" className="btn btn-circle">❯</a>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,52 @@
+//import { useContext } from "react";
+import { Link } from "react-router-dom";
+import logo from '../assets/image/logo.jpg'
+
+
+const Navbar = () => {
+    // const {user,logOut}=useContext(AuthContext);
+    // const handleLogOut=()=>{
+    //   logOut()
+    //   .then(()=>{})
+    //   .catch(error=>console.log(error))
+    // }
+      const navItems=<>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link> </li>
+      {/* {
+        user?.email?<>
+        <li><Link><button onClick={handleLogOut}>LogOut</button></Link></li>
+        <li><Link to='booking'>My Bookings</Link></li></>:<li><Link to='/login'>Login</Link></li>
+      } */}
+      
+      
+     
+      </>
+    return (
+        <div className="navbar bg-[#FFFFFF30] h-24 ">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow  bg-base-100 rounded-box w-52">
+            {navItems}
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            <img src={logo} className=' w-28 h-14' alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+       {navItems}
+          </ul>
+        </div>
+        <div className="navbar-end">
+        <button className="btn btn-outline btn-warning">Appointment</button>
+        </div>
+      </div>
+    );
+};
+
+export default Navbar;
