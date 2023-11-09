f7dc36937d40aec2654d62cdcd118aa7bc3f0e19
tafiya <tafiapinkey@gmail.com>
2023-11-09 18:02:14 +0600

    animation added in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,13 +3,17 @@ import Faq from "./Faq";
 import Feature from "./Feature";
 import Team from "./Team";
 
+
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+          
+                <Banner></Banner>
             <Feature></Feature>
             <Faq></Faq>
            <Team></Team>
+     
+          
         </div>
     );
 };
--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,6 +2,8 @@ import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 import axios from "axios";
+import { motion } from "framer-motion";
+
 
 
 
@@ -68,7 +70,18 @@ const AuthProvider = ({children}) => {
 
     return (
         <AuthContext.Provider value={authInfo}>
+            <motion.dev Variants ={
+                {
+                    hidden:{opacity: 0,y:75},
+                    visible:{opacity:1, y:0}
+                }
+            }
+            initial="hidden"
+            animate="visible"
+            transition={{duration:0.5, delay:0.25}} >
             {children}
+            </motion.dev>
+          
         </AuthContext.Provider>
     );
 };
